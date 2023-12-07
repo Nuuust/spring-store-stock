00cd7df61bdb7e4d026e8236132c2a5f7b3dd580
Alexandre Clenet <alexandreclenet@free.fr>
2023-12-07 09:54:45 +0100

    user gradle

---
+++ user/src/main/kotlin/storeStock/user/FrApplication.kt
@@ -0,0 +1,11 @@
+package storeStock.user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrApplication
+
+fun main(args: Array<String>) {
+	runApplication<FrApplication>(*args)
+}
---
+++ user/src/main/kotlin/storeStock/user/domain/User.kt
@@ -0,0 +1,2 @@
+package storeStock.user.domain
+data class User(val email: String, val firstName: String, val lastName: String, val age: Int)
