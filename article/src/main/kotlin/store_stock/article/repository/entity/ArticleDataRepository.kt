82d9b0b645eceb8c48d19d94bcc08684b647d6cf
Benjamin COUET <nust660@gmail.com>
2023-12-07 12:23:55 +0100

    article controller

---
+++ article/src/main/kotlin/store_stock/article/repository/entity/ArticleDataRepository.kt
@@ -0,0 +1,43 @@
+package store_stock.article.repository.entity
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import store_stock.article.domain.Article
+import store_stock.article.repository.ArticleRepository
+import store_stock.user.controller.dto.asArticleDTO
+import java.util.UUID
+import store_stock.article.repository.entity.ArticleEntity
+
+@Repository
+class ArticleDataRepository(private val jpa:ArticleJpaRepository):ArticleRepository {
+    override fun create(article:Article): Result<Article> = if (jpa.findById(article.id).isPresent) {
+        Result.failure(Exception("Article already in DB"))
+    } else {
+        val saved = jpa.save(article.asEntity())
+        Result.success(saved.asArticle())
+    }
+
+    override fun list(price:Float?): List<Article> {
+        return if (price == null) {
+            jpa.findAll().map { it.asArticle() }
+        } else {
+            jpa.findAllByPrice(price).map { it.asArticle() }
+        }
+    }
+
+    override fun get(id: UUID): Article? {
+        return jpa.findById(id)
+            .map { it.asArticle() }.get()
+    }
+
+    override fun update(article: Article): Result<Article> {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(id: UUID): Article? {
+        TODO("Not yet implemented")
+    }
+}
+interface ArticleJpaRepository : JpaRepository<ArticleEntity, UUID> {
+    fun findAllByPrice(price: Float): List<ArticleEntity>
+}
