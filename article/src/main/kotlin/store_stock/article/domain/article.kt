1ef08e103fb2b2dddf9e5e051f1f8f7410e094d5
Benjamin COUET <nust660@gmail.com>
2023-12-07 09:44:22 +0100

    Article entity

--- article/src/main/kotlin/store_stock/article/domain/article.kt
+++ article/src/main/kotlin/store_stock/article/domain/article.kt
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.PastOrPresent
 import jakarta.validation.constraints.PositiveOrZero
 import jakarta.validation.constraints.Size
-
+//test
 @Entity
 @Table(name = "Article")
 data class article(
