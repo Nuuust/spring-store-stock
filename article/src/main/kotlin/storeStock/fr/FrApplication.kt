74d7b728ee04bbe65bb42a0b260080e9656ec68d
Benjamin COUET <nust660@gmail.com>
2023-12-07 09:02:09 +0100

    article

---
+++ article/src/main/kotlin/storeStock/fr/FrApplication.kt
@@ -0,0 +1,11 @@
+package storeStock.fr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrApplication
+
+fun main(args: Array<String>) {
+	runApplication<FrApplication>(*args)
+}
